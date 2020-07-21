30332e2fdc67bc32e254f57f9a8d4427b20c4771
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2020-07-21 18:15:02 +0000

    made test coverage pass

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 
 
 setup(
-    name="CHANGE_THIS_TO_NAME_OF_PACKAGE",
+    name="change_this_to_name_of_package",
     version="0.1",
     description="CREATE A DESCRIPTION",
     url="https://git-codecommit.us-east-1.amazonaws.com/v1/repos/CHANGE_THIS_TO_NAME_OF_REPO",
     author="NanoSurface Biomedical",
-    author_email="eli@nanosurfacebio.com",
+    author_email="contact@curibio.com",
     license="MIT",
     packages=find_packages("src"),
     package_dir={"": "src"},
