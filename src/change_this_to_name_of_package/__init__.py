0db225fbfe87988d954cb8a670616d31c2a4254d
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2020-09-02 18:51:13 +0000

    added no cover exclusion for python38 in __init__

--- src/change_this_to_name_of_package/__init__.py
+++ src/change_this_to_name_of_package/__init__.py
@@ -3,7 +3,7 @@
 
 try:  # adapted from https://packaging.python.org/guides/single-sourcing-package-version/
     from importlib import metadata
-except ImportError:
+except ImportError:  # pragma: no cover
     # Running on pre-3.8 Python; use importlib-metadata package
     import importlib_metadata as metadata  # type: ignore # Eli (9/1/20): for some reason mypy is giving weird errors for this
 __version__: str = metadata.version("change_this_to_name_of_package")  # type: ignore # Eli (9/1/20): for some reason mypy is giving weird errors for this
