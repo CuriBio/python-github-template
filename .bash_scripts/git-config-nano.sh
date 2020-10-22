448244e8aac8d2913e7047ba886985ff5bc2d4b7
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2020-10-22 13:09:31 +0000

    updated github workflow

---
+++ .bash_scripts/git-config-nano.sh
@@ -0,0 +1,2 @@
+# In Cloud9, everytime the EC2 reboots, it seems like you need to explicitly set nano as the editor in order to allow interactive rebasing to work
+git config --global core.editor "nano"
---
+++ .bash_scripts/remove-local-git-branchs-that-are-gone-on-remote.sh
@@ -0,0 +1,4 @@
+# check remote for any updates and prune old branches deleted on remote
+git fetch --prune
+# delete local branches that are listed as "gone" on remote
+git branch -vv | grep ': gone]'|  grep -v "\*" | awk '{ print $1; }' | xargs -r git branch -d
